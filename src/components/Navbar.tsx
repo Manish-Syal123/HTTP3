6c6688660f500f63853444a1733f6d3396be8761
Manish_Syal <manishsyal77@gmail.com>
2025-01-13 17:06:10 +0530

    Wrote the Smart Contract and deployed the contract on BitTorrent. Used Network: BitTorrent Chain Donau for testing.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -73,7 +73,7 @@ export default function Navbar() {
 
   return (
     <>
-      <nav className="flex justify-between items-center mb-1 py-2 pt-4 px-4 md:px-8 lg:px-16">
+      <nav className="flex justify-between items-center mb-1 py-2 pt-4 px-4 md:px-8 lg:px-16 relative z-20">
         <Link href="/" className="flex items-center">
           <Image
             src="/svg/logo.svg"
